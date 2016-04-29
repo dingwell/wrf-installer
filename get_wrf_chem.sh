5409deafbc40c9b0847d7d742ec461c4885f4135
Adam Dingwell <adam@tintin1.uppmax.uu.se>
2016-04-29 11:18:32 +0200

    Test for existing WRFV3 directory

--- get_wrf_chem.sh
+++ get_wrf_chem.sh
@@ -48,6 +48,11 @@ SEP2="$B--------------------------------------------------$W"
 echo "WRF will be installed under $(pwd)/$WRF_DIR"
 
 init_tests () {
+  if [[ -e WRFV3 ]]; then
+    echo -e "${R}ERROR: Target directory 'WRFV3' already exists${D}"
+    echo -e "${R}       Please rename or delete before running installer${D}"
+    exit 1
+  fi
   if [[ -e $WRF_DIR ]]; then
     echo -e "${R}ERROR: Target directory '$WRF_DIR' already exists${D}"
     echo -e "${R}       Please rename or delete before running installer${D}"
